f7ea9fd17416ada64b3be37067ac36f49706aa82
Jamison <aligoogle@163.com>
2025-12-23 13:37:10 +0800

    feat: pwa

--- vue.config.js
+++ vue.config.js
@@ -18,6 +18,32 @@ module.exports = defineConfig({
     },
   }, //修改favicon.ico
   pwa: {
+    name: 'My Icon',
+    themeColor: '#409EFF',
+    msTileColor: '#409EFF',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      name: 'My Icon 图标库',
+      short_name: 'My Icon',
+      description: '提供在线图标链接，用于个人NAS设备显示使用',
+      start_url: './',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#409EFF',
+      icons: [
+        {
+          src: './Myicon.svg',
+          sizes: '192x192',
+          type: 'image/svg+xml'
+        },
+        {
+          src: './Myicon.svg',
+          sizes: '512x512',
+          type: 'image/svg+xml'
+        }
+      ]
+    },
     iconPaths: {
       favicon32: "Myicon.svg",
       favicon16: "Myicon.svg",
@@ -25,5 +51,52 @@ module.exports = defineConfig({
       maskIcon: "Myicon.svg",
       msTileImage: "Myicon.svg",
     },
+    // 配置 workbox
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // 跳过等待，立即激活新的 service worker
+      skipWaiting: true,
+      // 立即控制所有打开的页面
+      clientsClaim: true,
+      // 运行时缓存策略
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'google-fonts-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365 // 1年
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images-cache',
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 * 30 // 30天
+            }
+          }
+        },
+        {
+          urlPattern: /\.(?:js|css|html)$/i,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'static-resources',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24 // 1天
+            }
+          }
+        }
+      ]
+    }
   }
 })
